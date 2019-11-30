8b99bbd4055679bf5065891fa2aee0a412138d29
Julien Bisconti <julienbi@unity3d.com>
2019-11-30 13:41:37 -0700

    complete new topic form

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,11 +3,15 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # struct = %Discuss.Topic{}
     # params = %{}
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    
   end
 end
