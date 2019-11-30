f4151f230306578b71f82656d1ade919dab5edf3
Julien Bisconti <julienbi@unity3d.com>
2019-11-30 16:51:15 -0700

    edit functionality for topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -15,7 +15,7 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> 
+      {:ok, _topic} -> 
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
@@ -28,5 +28,26 @@ defmodule DiscussWeb.TopicController do
     topics = Repo.all(Topic)
 
     render conn, "index.html", topics: topics
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset = Repo.get(Topic, topic_id)
+    |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} -> 
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} -> 
+        render conn, "edit.html", changeset: changeset, topic: Repo.get(Topic, topic_id)
+    end
   end
 end
