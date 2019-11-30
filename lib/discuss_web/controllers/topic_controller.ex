bf1c8e66406757d418b3d4f2a557cfe951a351d6
Julien Bisconti <julienbi@unity3d.com>
2019-11-30 15:44:38 -0700

    topic index

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -15,9 +15,18 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
     end
+  end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
   end
 end
